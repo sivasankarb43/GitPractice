7fed0d25222e125113fd003d8da49a23c999b581
sivasankar <sivasankarb43@gmail.com>
2025-07-15 15:30:34 +0530

    sqash check new already pending pull res

--- sample.java
+++ sample.java
@@ -1,5 +1,9 @@
 public class sample {
     public static void main(String[] args) {
         int i = 0;
+        String Name="jhwtfd";
+        int c=1;
+        int h =2;
+        int new_1 = 3;
     }
 }
